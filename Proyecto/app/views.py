6c7b5faeb9f54202ea515d83d7c75d6b3566c5cc
Andrés Calderón Guardia <andres.calderon@ug.uchile.cl>
2024-12-06 03:08:49 -0300

    Add icon and handle edge cases

--- Proyecto/app/views.py
+++ Proyecto/app/views.py
@@ -15,25 +15,27 @@ def index(request):
 		return render(request, 'app/home.html')
 
 def register(request):
+	if request.method == 'GET':
+		form = RegistroForm()
+		return render(request, 'app/register.html', {'form': form})
+
 	if request.method == 'POST':
 		form = RegistroForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			login(request, user)
 			messages.success(request, "¡Registro exitoso! Has iniciado sesión.")
-			return redirect('chat')
+			return redirect('login2')
 		else:
 			messages.error(request, "Hubo un error en el formulario.")
-	else:
-		form = RegistroForm()
-
-	return render(request, 'app/register.html', {'form': form})
+			return render(request, 'app/register.html', {'form': form})
 
 def login2(request):
 	if request.method == 'GET':
 		return render(request, 'app/login.html')
 
 	if request.method == 'POST':
+		form = RegistroForm(request.POST)
 		username = request.POST['username']
 		password = request.POST['password']
 		user = authenticate(request, username=username, password=password)
@@ -44,11 +46,11 @@ def login2(request):
 			return redirect('home')
 		else:
 			messages.error(request, "Nombre de usuario o contraseña incorrectos.")
+			return render(request, 'app/login.html', {'form': form})
 
 def logout2(request):
 	logout(request)
 	return redirect('login2')
-
 
 @login_required(login_url='/login/')
 def home(request):
@@ -57,13 +59,13 @@ def home(request):
 
 @login_required(login_url='/login/')
 def chat(request, usuario_id):
+	clave_secreta = generar_clave()
 	receptor = User.objects.get(id=usuario_id)
 	if request.method == 'POST':
 		form = MensajeForm(request.POST)
 		if form.is_valid():
 			# Encriptamos el mensaje
 			mensaje_original = form.cleaned_data['mensaje']
-			clave_secreta = generar_clave()
 			mensaje_cifrado = cifrar_mensaje(mensaje_original, clave_secreta)
 			mensaje_base64 = base64.b64encode(mensaje_cifrado).decode()
 
@@ -72,7 +74,7 @@ def chat(request, usuario_id):
 			mensaje_completo = mensaje_base64 + hmac_secreto
 			
 			# Ocultamos el mensaje en la imagen
-			imagen_path = "imagen_original.png"  # La imagen original
+			imagen_path = "imagen_original.png"
 			ocultar_mensaje_imagen(imagen_path, mensaje_completo)
 			
 			# Guardamos en la base de datos (se guarda la imagen, pero solo el ID de la imagen o path)
@@ -81,7 +83,7 @@ def chat(request, usuario_id):
 			return redirect('chat', usuario_id=usuario_id)
 	else:
 		form = MensajeForm()
-	
+
 	# Mostramos los mensajes enviados y recibidos
 	mensajes = Mensaje.objects.filter(
 		(models.Q(remitente=request.user) & models.Q(receptor=receptor)) | 
@@ -92,8 +94,9 @@ def chat(request, usuario_id):
 	for mensaje in mensajes:
 		# Extraemos el mensaje y lo verificamos
 		longitud_bits = len(mensaje.mensaje) * 8
+		imagen_path = "imagen_oculta.png"
 		mensaje_extraido = extraer_mensaje_imagen(imagen_path, longitud_bits)
-		
+
 		# Extraemos el mensaje cifrado y el HMAC
 		mensaje_cifrado_extraido = mensaje_extraido[:-64]
 		hmac_extraido = mensaje_extraido[-64:]
@@ -103,5 +106,5 @@ def chat(request, usuario_id):
 			mensajes_descifrados.append(mensaje_descifrado)
 		else:
 			mensajes_descifrados.append("Error: El HMAC no coincide. El mensaje ha sido alterado.")
-	
+
 	return render(request, 'app/chat.html', {'form': form, 'mensajes_descifrados': mensajes_descifrados, 'receptor': receptor})
